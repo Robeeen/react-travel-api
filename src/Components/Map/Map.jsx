4bcbdc1fb2b4b4b9aabe145d13d33441fdef523a
Shams Arefin Khan <robeeen@gmail.com>
2022-01-15 13:34:30 +0600

    Udpates

--- src/Components/Map/Map.jsx
+++ src/Components/Map/Map.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from 'react';
+import GoogleMapReact from 'google-map-react';
+import { Paper, Typography, useMediaQuery } from '@material-ui/core';
+import LocationOutlinedIcon from '@material-ui/icons/LocationOutlined';
+import Rating from '@material-ui/lab';
+import useStyles from './style';
+
 
 const Map = () => {
+    const classes = useStyles();
+    const isMobile = useMediaQuery('(min-width: 600px)');
+    const coordinates = { lat: 0, lng: 0};
     return (
-        <div>
-            
+        <div className={classes.mapContainer}>
+            <GoogleMapReact
+                bootstrapURLKeys={{ key: ''}}
+                defaultCenter={coordinates}
+                center={coordinates}
+                defaultZoom={14}
+                margin={50, 50, 50, 50}
+                position={''}
+                onChange={''}
+                onChildClick={''}
+            >
+
+            </GoogleMapReact>            
         </div>
     )
 }
